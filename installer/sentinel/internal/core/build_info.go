277b09a1c4bf11f60916de103824bedc84ef8ef2
Jose Vigil <jose.vigil@roamtouch.com>
2026-02-04 21:40:48 -0300

    Fixed Seed Command

--- installer/sentinel/internal/core/build_info.go
+++ installer/sentinel/internal/core/build_info.go
@@ -1,8 +1,8 @@
 package core
 
 // Auto-generated during build - DO NOT EDIT
-// Generated: 2026 -Wed 02-04 21:08:00
+// Generated: 2026 -Wed 02-04 21:36:00
 
-const BuildNumber = 127
+const BuildNumber = 128
 const BuildDate = "2026 -Wed 02-04"
-const BuildTime = "21:08:00"
+const BuildTime = "21:36:00"
