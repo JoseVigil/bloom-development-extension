3e0bb92425bd7eee7ec8b1915f3d6da1e4140862
Jose Vigil <jose.vigil@roamtouch.com>
2026-05-03 17:47:16 -0300

    fixed metamorph build

--- installer/metamorph/internal/core/build_info.go
+++ installer/metamorph/internal/core/build_info.go
@@ -1,6 +1,6 @@
 package core
 
 // Auto-generated during build
-const BuildNumber = 11
+const BuildNumber = 12
 const BuildDate = "2026 -Sun 05-03"
-const BuildTime = "16:15:00"
+const BuildTime = "17:46:00"
